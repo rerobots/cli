6cb4e14bf02c50ea8f4d62c9910af4eab06c6851
Scott C. Livingston <scott@rerobots.net>
2021-07-18 15:24:14 -0700

    dep: openssl-probe
    https://crates.io/crates/openssl-probe
    https://github.com/alexcrichton/openssl-probe
    
    motivated for static linking

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ mod client;
 
 
 fn main() {
+    #[cfg(unix)]
+    {
+        openssl_probe::init_ssl_cert_env_vars();
+    }
     match cli::main() {
         Ok(_) => std::process::exit(0),
         Err(err) => {
