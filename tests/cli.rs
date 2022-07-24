b55dc1b53673b4808c26f671dc000fac31efcd33
Scott C. Livingston <scott@rerobots.net>
2022-07-24 16:53:41 -0700

    TEST: snapshot `help`

--- tests/cli.rs
+++ tests/cli.rs
@@ -12,3 +12,13 @@ fn prints_version() {
         .stdout(format!("{}\n", env!("CARGO_PKG_VERSION")))
         .success();
 }
+
+
+#[test]
+fn prints_help() {
+    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let assert = cmd.arg("help").assert();
+    let output = assert.get_output().clone();
+    assert.success();
+    insta::assert_display_snapshot!(String::from_utf8(output.stdout).unwrap());
+}
