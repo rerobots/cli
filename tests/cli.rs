c5ccf2a6213986dd06550a5f243981b448338338
Scott C. Livingston <scott@rerobots.net>
2026-01-28 23:50:32 -0800

    TEST: refactor to use fewer unwrap()

--- tests/cli.rs
+++ tests/cli.rs
@@ -14,98 +14,89 @@
 
 use assert_cmd::Command;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn prints_version() {
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+fn prints_version() -> TestResult {
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("-V").assert();
     assert
         .stdout(format!("{}\n", env!("CARGO_PKG_VERSION")))
         .success();
+    Ok(())
 }
 
 #[test]
-fn prints_help() {
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+fn prints_help() -> TestResult {
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!(String::from_utf8(output.stdout)?);
 
     // Alternative style: -h
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("-h").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout)?);
 
     // Alternative style: --help
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("--help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout)?);
+
+    Ok(())
 }
 
 #[test]
-fn prints_help_search() {
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+fn prints_help_search() -> TestResult {
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("help").arg("search").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_search",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_search", String::from_utf8(output.stdout)?);
 
     // Alternative style: -h
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("search").arg("-h").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_search",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_search", String::from_utf8(output.stdout)?);
 
     // Alternative style: --help
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("search").arg("--help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_search",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_search", String::from_utf8(output.stdout)?);
+
+    Ok(())
 }
 
 #[test]
-fn prints_help_launch() {
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+fn prints_help_launch() -> TestResult {
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("help").arg("launch").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_launch",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_launch", String::from_utf8(output.stdout)?);
 
     // Alternative style: -h
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("launch").arg("-h").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_launch",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_launch", String::from_utf8(output.stdout)?);
 
     // Alternative style: --help
-    let mut cmd = Command::cargo_bin("rerobots").unwrap();
+    let mut cmd = Command::cargo_bin("rerobots")?;
     let assert = cmd.arg("launch").arg("--help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_launch",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_launch", String::from_utf8(output.stdout)?);
+
+    Ok(())
 }
